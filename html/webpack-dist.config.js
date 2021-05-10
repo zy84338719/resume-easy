6c6fc71fa79b052094e2c36a5a5ede3acbbd80f2
murphyyi <zy84338719@hotmail.com>
2021-05-10 20:24:08 +0800

    add 简历

--- html/webpack-dist.config.js
+++ html/webpack-dist.config.js
@@ -54,13 +54,13 @@ module.exports = {
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.murphyyi.com');
+      // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.murphyyi.com');
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.murphyyi.com' // 这里注意改成你的在线简历的网站
-      ]);
+      // spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
+      //   'http://resume.murphyyi.com' // 这里注意改成你的在线简历的网站
+      // ]);
     }),
   ]
 };
--- main.go
+++ main.go
@@ -3,51 +3,61 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/zy84338719/resume-easy/tools"
+	"github.com/zy84338719/resume-easy/tools" //nolint:gci
 	"html"
 	"html/template"
-	"os"
+	"os" //nolint:gci
+)
+
+const (
+	DATA_READ   = "data.json"
+	CREATE_DATA = "html/src/index.html"
+	TEMPLATE    = "tools/index.tpl"
 )
 
 func main() {
-
 	s := tools.NewHtml()
 	data, err := readData()
-	if err!=nil{
+	if err != nil {
 		return
 	}
-	err=json.Unmarshal(data,&s)
-	if err!=nil{
-		fmt.Println("数据加载失败err=",err)
+	err = json.Unmarshal(data, &s) //nolint:wsl
+	if err != nil {                //nolint:wsl
+		fmt.Println("数据加载失败err=", err)
 	}
 	s.Now()
 	loadTemplate(s)
-
 }
-func loadTemplate(data *tools.Html){
-	file,err := os.Create("html/src/index.html")
-	if err!=nil{
-		fmt.Printf("创建文件异常 err=%+v",err)
+func loadTemplate(data *tools.Html) {
+	file, err := os.Create(CREATE_DATA)
+	if err != nil {
+		fmt.Printf("创建文件异常 err=%+v", err) //nolint:forbidigo
 	}
-	if file!=nil{
+	if file != nil {
 		defer file.Close()
 	}
-	tpl:=template.Must(template.ParseFiles("tools/index.html"))
-	if err:=tpl.Execute(file, data);err!=nil{
-		fmt.Printf("写入出现异常，err=%+v \n",err)
+	tpl := template.Must(template.ParseFiles(TEMPLATE))
+	if err := tpl.Execute(file, data); err != nil {
+		fmt.Printf("写入出现异常，err=%+v \n", err)
 		return
 	}
-	r, _ := os.ReadFile("html/src/index.html")
+	r, err := os.ReadFile("html/src/index.html")
+	if err != nil {
+		fmt.Printf("读取数据失败 err=%+v", err)
+	}
 	s := html.UnescapeString(string(r))
-	os.WriteFile("html/src/index.html",[]byte(s),0755)
-	fmt.Println("数据写入成功")
+	err = os.WriteFile(CREATE_DATA, []byte(s), 0755)
+	if err != nil {
+		fmt.Printf("写入数据失败 err=%+v", err) //nolint:forbidigo
+	}
+	fmt.Println("数据写入成功") //nolint:wsl,forbidigo
 }
 
-func readData()([]byte,error){
-	file, err := os.ReadFile("data.json")
-	if err!=nil{
-		fmt.Printf("读取数据失败,err=%+v \n" ,err)
-		return nil,err
+func readData() ([]byte, error) {
+	file, err := os.ReadFile(DATA_READ)
+	if err != nil {
+		fmt.Printf("读取数据失败,err=%+v \n", err)
+		return nil, err
 	}
-	return file,err
+	return file, err
 }
